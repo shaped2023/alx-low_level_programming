26cefc12ce29c5b8311acf0a15378ab6a006b48d
shaped2023 <shalewapedro@yahoo.com>
2023-02-20 05:34:09 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
 *main - Entry point
-*Return: Always 0 (Success)
+*Return: always 0 (success)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle);
-return (0);
+	puts("Z"Programming is like building a multilingual puzzle");
+	return (0);
 }
-	
+
