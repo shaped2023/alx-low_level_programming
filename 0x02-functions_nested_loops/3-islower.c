cad1994ea95c79e0994f622af97a60b1c9b1863c
shaped2023 <shalewapedro@yahoo.com>
2023-02-21 16:51:19 -0800

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase letter
+ * @c: character to check case
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+    return (c >= 97 && c <= 122);
+}
