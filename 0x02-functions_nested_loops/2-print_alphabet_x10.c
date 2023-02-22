acd749eecf4c13635f72cb96af7b0e2c12e3415c
shaped2023 <shalewapedro@yahoo.com>
2023-02-21 17:15:09 -0800

    2-print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print alphabet_x10 - prints alphabet 10 time
+ * Return: void
+ */
+
+int main(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++
+		}
+		_putchar('\n');
+		i++;
+	}
+}
