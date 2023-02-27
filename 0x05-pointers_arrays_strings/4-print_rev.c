f1857397ece80e6ff5d598973a068a151263ae6e
shaped2023 <shalewapedro@yahoo.com>
2023-02-27 12:52:25 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: input string
+ * Return: String in reverse
+ */
+void point_rev(char *s)
+{
+	int longi = 0;
+	int 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
+	
