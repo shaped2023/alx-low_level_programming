849b178870d8ed707af1a7c5cf937e2a66046b15
shaped2023 <shalewapedro@yahoo.com>
2023-02-27 08:56:00 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,16 @@
-#include "main.h"
+#include main.h
 /**
- * swap_int - swaps the values of two integers
- * @a: integer to swap
- * @b: integer to swap
+ * swap_int - swap variable value
+ * @a:pointera
+ * @b:pointerb
+ * Return: void
  */
+
 void swap_int(int *a, int *b)
 {
-	int m;
-
-	m = *a;
-	*a = *b;
-	*b = m;
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
 
 
