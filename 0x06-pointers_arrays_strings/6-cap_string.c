45d088508678f567a68d5a2124980728aff7b6aa
shaped2023 <shalewapedro@yahoo.com>
2023-03-01 14:28:23 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: The string to be capitalized
+ *
+ * Return: A pointer to the changed string
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'n' && str[index] <= 'z'))
+			index++;
+		if(str
+	}
+}
